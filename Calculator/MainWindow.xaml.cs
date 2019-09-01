f66d61a417a09322546f6bf292590dcfa82da479
Devil7DK <dineshthangavel47@gmail.com>
2019-09-01 14:31:46 +0530

    Add Handler for Number Buttons

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,11 +1,17 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace Calculator
 {
     public class MainWindow : Window
     {
+
+#region Controls
+        private TextBox txtInput;
+#endregion
+
         public MainWindow()
         {
             InitializeComponent();
@@ -14,6 +20,13 @@ namespace Calculator
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+
+            this.txtInput = this.Get<TextBox>("txtInput");
+        }
+
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            this.txtInput.Text = this.txtInput.Text + ((Button)sender).Content.ToString();
         }
     }
 }
